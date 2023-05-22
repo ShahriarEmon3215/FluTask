8765730f73dee556a6a0dcef436db2d369e95a88
ShahriarEmon3215 <shahriar3215emon@gmail.com>
2023-05-22 22:21:57 +0600

    initial commit

---
+++ android/app/src/main/java/com/emon/app/flutask/MainActivity.java
@@ -0,0 +1,6 @@
+package com.emon.app.flutask;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/controllers/theme_controller.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.indigo,
+    buttonTheme: ButtonThemeData(
+      buttonColor: Colors.indigo,
+      textTheme: ButtonTextTheme.primary,
+    ),
+    scaffoldBackgroundColor: Color(0xfff1f1f1));
+
+ThemeData dark = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.dark,
+  primarySwatch: Colors.indigo,
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.red,
+    textTheme: ButtonTextTheme.primary,
+  ),
+);
+
+class ThemeNotifier extends ChangeNotifier {
+  final String key = "theme";
+  SharedPreferences? prefs;
+  bool? _darkTheme;
+
+  bool? get darkTheme => _darkTheme!; //Getter
+
+  ThemeNotifier() {
+    _darkTheme = false;
+    loadFromPrefs();
+  }
+
+  toggleTheme() {
+    _darkTheme = !_darkTheme!;
+    saveToPrefs();
+    notifyListeners();
+  }
+
+  _initPrefs() async {
+    if (prefs == null) prefs = await SharedPreferences.getInstance();
+  }
+
+  loadFromPrefs() async {
+    await _initPrefs();
+    _darkTheme = prefs!.getBool(key) ?? true;
+    notifyListeners();
+  }
+
+  saveToPrefs() async {
+    await _initPrefs();
+    prefs!.setBool(key, darkTheme!);
+  }
+}
